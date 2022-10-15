46fbf2740b132f946ff81c043d391c40bad2c87f
Cleiton Deusdara <cleiton.deusdara@gmail.com>
2022-10-15 16:40:24 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import NotificatioButton from "./components/NotificationButton"
-
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+  
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificatioButton />
-      <NotificatioButton />
+      <Header/>
+      <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard />
+          
+        </div>
+        </section>
+        </main>
     </>
   )
 }
