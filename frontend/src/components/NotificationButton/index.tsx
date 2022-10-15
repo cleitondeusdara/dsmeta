95f9fa3514b2bfe2eecc8d8c1f6c482064542091
Cleiton Deusdara <cleiton.deusdara@gmail.com>
2022-10-15 01:12:36 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/notification_icon.svg'
+import './Styles.css'
+function NotificatioButton() {
+    return (
+
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+
+    )
+}
+
+export default NotificatioButton
